067eb5f60c72d132ee6edea2edc9c6d54fb7ff6a
Hugh Willson <hugh@octonary.com>
2017-11-13 11:37:29 -0500

    Adjust error handling

--- imports/sentry.js
+++ imports/sentry.js
@@ -1,17 +1,21 @@
 /* eslint-disable no-console */
 
-const sentry = {};
+let sentry;
 
 if (process.env.SENTRY_DSN) {
   import raven from 'raven';
   raven.config(process.env.SENTRY_DSN).install();
-  Object.assign(sentry, raven);
+  sentry = raven;
 } else {
   console.warn(
     'WARNING: No SENTRY_DSN environment variable found; ' +
     'sentry.io reporting is disabled.',
   );
-  sentry.captureException = error => error;
+  sentry = {
+    captureException(error) {
+      return error;
+    },
+  };
 }
 
 export default sentry;
